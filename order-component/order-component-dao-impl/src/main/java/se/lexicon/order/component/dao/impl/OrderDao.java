cdd9f93d59e4d165ced01fe7c2d2068ab492c6b4
Medasele273 <medasele273@gmail.com>
2019-08-09 00:41:22 +0200

    deleting classes

--- order-component/order-component-dao-impl/src/main/java/se/lexicon/order/component/dao/impl/OrderDao.java
+++
@@ -1,7 +0,0 @@
-package se.lexicon.order.component.dao.impl;
-
-import com.so4it.component.GenericDao;
-import se.lexicon.order.component.entity.OrderEntity;
-
-public interface OrderDao extends GenericDao<OrderEntity,String> {
-}
