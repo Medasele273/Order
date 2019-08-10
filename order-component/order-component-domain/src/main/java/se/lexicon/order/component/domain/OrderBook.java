246e35769a70e8605b056b22a0966b921b6da7a0
Medasele273 <medasele273@gmail.com>
2019-08-10 17:01:15 +0200

    OrderBooks class is added

---
+++ order-component/order-component-domain/src/main/java/se/lexicon/order/component/domain/OrderBook.java
@@ -0,0 +1,4 @@
+package se.lexicon.order.component.domain;
+
+public class OrderBook {
+}
--- order-component/order-component-domain/src/main/java/se/lexicon/order/component/domain/OrderBooks.java
+++ order-component/order-component-domain/src/main/java/se/lexicon/order/component/domain/OrderBooks.java
@@ -1,5 +1,52 @@
 package se.lexicon.order.component.domain;
 
-public class OrderBooks {
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.so4it.common.domain.AbstractIterable;
+import com.so4it.common.domain.AbstractIterableDeserializer;
+import com.so4it.common.domain.AbstractIterableSerializer;
+import com.so4it.common.domain.DomainClass;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+
+@DomainClass
+@JsonDeserialize(
+        using = OrderBooks.Deserialize.class
+)
+@JsonSerialize(
+        using = OrderBooks.Serialize.class
+)
+public class OrderBooks extends AbstractIterable<OrderBook> {
+
+    public OrderBooks(){
+        super(OrderBook.class,OrderBooks.class);
+    }
+
+    public static OrderBooks valueOf(OrderBook... endpointRegistrations){
+        return (OrderBooks) AbstractIterable.valueOf(endpointRegistrations,OrderBook.class,OrderBooks.class);
+    }
+
+    public static OrderBooks valueOf(Set<OrderBook> endpointRegistrations){
+        return (OrderBooks) AbstractIterable.valueOf(endpointRegistrations,OrderBook.class,OrderBooks.class);
+    }
+
+    public static OrderBooks valueOf(Iterable<OrderBook> endpointRegistrations){
+        return (OrderBooks) AbstractIterable.valueOf(endpointRegistrations,OrderBook.class,OrderBooks.class);
+    }
+
+    public Map<String,OrderBooks> map(Function<OrderBook,String> mapper){return null;}
+
+    public static class Deserialize extends AbstractIterableDeserializer<OrderBook>{
+        protected Deserialize(){
+            super(OrderBook.class,OrderBooks.class);
+        }
+    }
+
+    public static class Serialize extends AbstractIterableSerializer<OrderBook>{
+        protected Serialize(){
+            super(OrderBook.class,OrderBooks.class);
+        }
+    }
 }
