b37b98bf095116391922ab6de922c95685d54320
Medasele273 <medasele273@gmail.com>
2019-08-10 16:02:22 +0200

    Money class is added to the Domain

--- order-component/order-component-domain/src/main/java/se/lexicon/order/component/domain/Money.java
+++ order-component/order-component-domain/src/main/java/se/lexicon/order/component/domain/Money.java
@@ -1,12 +1,65 @@
 package se.lexicon.order.component.domain;
 
+import com.so4it.common.domain.DomainClass;
+import com.so4it.common.util.object.Required;
 import com.so4it.common.util.object.ValueObject;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Currency;
 
+@DomainClass
 public class Money extends ValueObject implements Serializable {
+    private static final long serialVersionUID=1l;
+
+    private BigDecimal amount;
+    private Currency currency;
+
+    private Money(){
+
+    }
+
+    private Money(Builder builder){
+        this.amount= Required.notNull(builder.amount,"amount");
+        this.currency= Required.notNull(builder.currency,"currency");
+    }
+
+    public BigDecimal getAmount(){
+        return amount;
+    }
+
+    public Currency getCurrency(){
+        return currency;
+    }
+
+
     @Override
     protected Object[] getIdFields() {
-        return new Object[0];
+        return new Object[]{amount,currency};
+    }
+
+    public static Builder builder(){
+        return new Builder();
+    }
+
+    public static class Builder implements com.so4it.common.builder.Builder<Money>{
+
+        private BigDecimal amount;
+        private Currency currency;
+
+        public Builder withAmount(BigDecimal amount){
+            this.amount=amount;
+            return this;
+        }
+
+        public Builder withCurrency(Currency currency){
+            this.currency=currency;
+            return this;
+        }
+
+        @Override
+        public  Money build(){
+            return new Money(this);
+        }
     }
 }
